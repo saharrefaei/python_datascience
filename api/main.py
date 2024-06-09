4dd543ea686ce30028cfd170a892cdd86949737e
Your Correct Username <saharrefaei1999@gmail.com>
2024-06-09 20:10:20 +0200

    pulynamial regression

--- api/main.py
+++ api/main.py
@@ -4,28 +4,46 @@ import pylab as pl
 import numpy as np 
 from sklearn import linear_model
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import PolynomialFeatures
 df = pd.read_csv(r"C:\Users\sahar\Downloads\FuelConsumptionCo2.csv")
 
 df.dropna()
+
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+
 
 random =  np.random.rand(len(df)) < 0.8
 
 train = df[random]
 test=df[~random]
 
-
-regression = linear_model.LinearRegression()
-
-train_x = np.asanyarray(train[['ENGINESIZE' , 'CYLINDERS','FUELCONSUMPTION_COMB']])
-train_y = np.asanyarray(train[['CO2EMISSIONS']])
-
-regression.fit(train_x,train_y)
-print('coef : tetta 1' , regression.coef_)
-print('tetta 0 : ' , regression.intercept_)
+train_x = np.asanyarray(train[['ENGINESIZE']]) 
+train_y = np.asanyarray(train[['CO2EMISSIONS']]) 
+test_x = np.asanyarray(test[['ENGINESIZE']]) 
+test_y = np.asanyarray(test[['CO2EMISSIONS']]) 
 
 
-test_y_ = regression.predict(test[['ENGINESIZE' , 'CYLINDERS','FUELCONSUMPTION_COMB']])
-test_x=np.asanyarray(test[['ENGINESIZE' , 'CYLINDERS','FUELCONSUMPTION_COMB']])
-test_y=np.asanyarray(test[['CO2EMISSIONS']])
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
 
-print('r2_score:', r2_score(test_y, test_y_))
+
+clf = linear_model.LinearRegression()
+clf.fit(train_x_poly,train_y)
+
+
+print("coef :",clf.coef_)
+print("intercept : " , clf.intercept_)
+
+
+
+XX = np.arange(0.0,10,0.1)
+
+YY = clf.intercept_[0] + clf.coef_[0][1] * XX +  clf.coef_[0][2] * np.power(XX,2)
+
+
+
+
+
+test_x_poly= poly.fit_transform(test_x)
+test_y_ = clf.predict(test_x_poly)
+print("R2-score: %.2f" % r2_score(test_y,test_y_ ) )
