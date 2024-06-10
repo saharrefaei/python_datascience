4b84230fbdc46b9b4a29a82fce8e4217b5d9ec36
Your Correct Username <saharrefaei1999@gmail.com>
2024-06-10 20:17:51 +0200

    make the mulit linear regression with R2 = 53%

--- api/main.py
+++ api/main.py
@@ -3,37 +3,68 @@ import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
-from sklearn.preprocessing  import PolynomialFeatures
+
+# خواندن داده‌ها
 df = pd.read_csv(r"C:\Users\sahar\Downloads\1632300362534233.csv")
 
-df.dropna(inplace=True)
-df['Price'].apply(lambda x: float(x))
+# تبدیل ستون 'Price' به float
+df['Price'] = df['Price'].apply(lambda x: float(x))
+
+# حذف ردیف‌هایی که مقدار ستون 'Area' آن‌ها '###' است
+df = df[df['Area'] != '###']
+
+# حذف کاما و تبدیل ستون 'Area' به float
+df['Area'] = df['Area'].str.replace(',', '').astype(float)
+
+# حذف مقادیر خالی از ستون 'Address'
+df['Address'].dropna()
+
+# تبدیل مقادیر True/False به 1/0
+df["Parking"] = df["Parking"].replace({True: 1, False: 0})
+df["Warehouse"] = df["Warehouse"].replace({True: 1, False: 0})
+df["Elevator"] = df["Elevator"].replace({True: 1, False: 0})
+
+# اعمال One-Hot Encoding بر روی ستون "Address"
+encoded_address = pd.get_dummies(df['Address'], prefix='Address')
+
+# اضافه کردن ستون‌های جدید به دیتافریم
+df = pd.concat([df, encoded_address], axis=1)
+
+# حذف ستون "Address" اصلی
+df.drop(columns=['Address'], inplace=True)
+
+# انتخاب ویژگی‌ها و قیمت به عنوان ورودی و خروجی مدل
+features = ["Area", "Room", "Parking", "Warehouse", "Elevator"] + list(encoded_address.columns)
+msk = df[features + ['Price']]
 
 
-msk = df[[ "Area",  "Room" , "Parking",'Price' , 'Price(USD)']]
-plt.scatter(df.Room , df.Price , color = 'green')
-plt.show()
-
+# تقسیم داده‌ها به مجموعه آموزشی و آزمایشی
 random = np.random.rand(len(msk)) < 0.8
 train = msk[random]
 test = msk[~random]
 
+# نرمال‌سازی ستون 'Area'
+x_data = df['Area'].values
+Area_normalized = x_data / max(x_data)
+df['Area'] = Area_normalized
 
-train_x = np.asanyarray(train[[ "Room" ]])
-train_y = np.asanyarray(train[['Price']]) 
-test_x = np.asanyarray(test[[ "Room" ]])
+# آماده‌سازی داده‌های آموزشی و آزمایشی
+train_x = np.asanyarray(train[features])
+train_y = np.asanyarray(train[['Price']])
+test_x = np.asanyarray(test[features])
 test_y = np.asanyarray(test[['Price']])
 
-
-poly = PolynomialFeatures(degree=2)
-train_x_poly = poly.fit_transform(train_x)
-# ایجاد و آموزش مدل رگرسیون خطی
+# ایجاد و آموزش مدل خطی
 regression = linear_model.LinearRegression()
-regression.fit(train_x_poly, train_y)
-print('coef :',regression.coef_)
-print("intercept :" , regression.intercept_)
+regression.fit(train_x, train_y)
 
-test_x_poly = poly.fit_transform(test_x)
-test_y_ = regression.predict(test_x_poly)
+# نمایش ضرایب و عرض از مبدا مدل
+print('coef:', regression.coef_)
+print("intercept:", regression.intercept_)
 
-print("r2score : " , r2_score(test_y,test_y_))
+# پیش‌بینی بر روی داده‌های آزمایشی
+test_y_pred = regression.predict(test_x)
+
+# محاسبه r2_score
+r2 = r2_score(test_y, test_y_pred)
+print("r2score:", r2)
