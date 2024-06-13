051f31361fa297c4d3142bc9f29621132d37033c
Your Correct Username <saharrefaei1999@gmail.com>
2024-06-13 20:26:16 +0200

    predict the illness with desission tree with 85% accuracy

--- api/main.py
+++ api/main.py
@@ -5,43 +5,58 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
-df = pd.read_csv(r"C:\Users\sahar\Downloads\drug200.csv")
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score
+df = pd.read_csv(r"C:\Users\sahar\Downloads\heart.csv")
 df.replace(['', ' ', 'NA', 'N/A', 'n/a', 'na'], np.nan, inplace=True)
 df = df.dropna()
-
-X = df[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
-y = df['Drug']
-
-
-
-le_sex = preprocessing.LabelEncoder()
-le_sex.fit(['F','M'])
-X[:,1] = le_sex.transform(X[:,1]) 
-
-
-le_sex.fit(['HIGH','NORMAL','LOW'])
-X[:,2] = le_sex.transform(X[:,2]) 
-
-
-le_sex.fit(['HIGH','NORMAL'])
-X[:,3] = le_sex.transform(X[:,3]) 
+X = df[['age' , 'sex' , 'cp'  ,'trtbps' , 'chol',  'fbs'  ,'restecg' , 'thalachh',  'exng' , 'oldpeak' , 'slp' , 'caa'  ,'thall']].values
+y = df['output']
 
 
 
+# normalize  = preprocessing.StandardScaler().fit(X)
+# X=normalize.transform(X.astype(float))
 
-print(X)
+train_x ,test_x, train_y , test_y = train_test_split(X,y,test_size=0.2 , random_state=4)
 
-x_train , x_test , y_train , y_test = train_test_split(X,y,test_size=0.2,random_state=4)
+DecisionTree = DecisionTreeClassifier(criterion='entropy',max_depth=4)
 
-print(x_train.shape , y_train.shape)
-print(x_test.shape , y_test.shape)
+DecisionTree.fit(train_x,train_y)
 
-# K=4
-desiionTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4).fit(x_train , y_train)
+predict_tree = DecisionTree.predict(test_x)
 
-
-yhat = desiionTree.predict(x_test)
-
-print("test set Accuracy: ",metrics.accuracy_score(y_test , yhat))
+print(predict_tree[0:5])
+print(test_y[0:5])
 
 
+accuracy = metrics.accuracy_score(test_y,predict_tree)
+
+print("acciracy : " , accuracy )
+
+conf_matrix = confusion_matrix(test_y, predict_tree)
+plt.figure(figsize=(8,6))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=['No', 'Yes'], yticklabels=['No', 'Yes'])
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.title('Confusion Matrix')
+plt.show()
+
+# محاسبه احتمالات پیش‌بینی مثبت
+predict_prob_tree = DecisionTree.predict_proba(test_x)[:, 1]
+
+# رسم منحنی ROC
+fpr, tpr, _ = roc_curve(test_y, predict_prob_tree)
+roc_auc = roc_auc_score(test_y, predict_prob_tree)
+
+plt.figure(figsize=(8,6))
+plt.plot(fpr, tpr, color='blue', label=f'ROC Curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='gray', linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc="lower right")
+plt.show()
